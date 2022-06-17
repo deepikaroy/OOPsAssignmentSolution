d4efb658ea5e4a523646f07940a3867ef87b6097
Deepika Roy <droy@csod.com>
2022-06-17 17:24:49 +0530

    Committing code

---
+++ GradedAssesment1/src/com/assessment/main/Main.java
@@ -0,0 +1,35 @@
+package com.assessment.main;
+
+import com.assessment.business.*;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{		
+		AdminDepartment admin = new AdminDepartment();	
+		
+		System.out.println("Welcome to " + admin.departmentName());
+		System.out.println(admin.getTodaysWork());
+		System.out.println(admin.getWorkDeadline());
+		System.out.println(admin.isTodayAHoliday());
+		System.out.println();
+		
+		HrDepartment hr = new HrDepartment();
+		
+		System.out.println("Welcome to " + hr.departmentName());
+		System.out.println((hr.doActivity()));
+		System.out.println(hr.getTodaysWork());
+		System.out.println(hr.getWorkDeadline());
+		System.out.println(hr.isTodayAHoliday());
+		System.out.println();
+		
+		TechDepartment tech = new TechDepartment();
+		
+		System.out.println("Welcome to " + tech.departmentName());
+		System.out.println(tech.getTodaysWork());
+		System.out.println(tech.getWorkDeadline());
+		System.out.println((tech.doActivity()));
+		System.out.println(tech.isTodayAHoliday());
+	}
+
+}
